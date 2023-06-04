29243af3872d03b52772593446ff037f76002e4e
chamalka <chamalkasandeepa@gmail.com>
2023-06-05 02:02:13 +0530

    ayuraveda_e_channeling_mobile_application -version 1.02

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     TextEditingController _searchController = TextEditingController();
-
 
     return Scaffold(
       backgroundColor: const Color(0xFFE9EDF5), // Set the background color
@@ -16,10 +13,12 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              margin: const EdgeInsets.only(left: 20, top:40), // Set the left margin
+              margin: const EdgeInsets.only(left: 20, top: 40),
+              // Set the left margin
               child: Row(
                 children: [
-                  SizedBox(width: 10), // Add some space between the text and the avatar
+                  SizedBox(width: 10),
+                  // Add some space between the text and the avatar
                   CircleAvatar(
                     radius: 25,
                     backgroundImage: AssetImage('assets/logo.png'),
@@ -35,15 +34,15 @@ class HomeScreen extends StatelessWidget {
                     padding: const EdgeInsets.only(left: 10),
                     child: Text(
                       'ආයුබෝවන් ! හෙළ සුව',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500
-                      ),
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                     ),
                   ),
                 ],
               ),
             ),
+
+            ///Search bar
             Container(
               margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
               child: Row(
@@ -74,6 +73,7 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
 
+            ///Popular Workouts Section
             Column(
               children: [
                 Padding(
@@ -89,7 +89,236 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-               
+                Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 20),
+                  height: 140,
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProfileScreen()),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 30, right: 5.0),
+                          child: Image.asset('assets/face.jpg'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProfileScreen()),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                          child:
+                              Image.asset('assets/images/back_stretching.png'),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProfileScreen()),
+                          );
+                        },
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.only(left: 5.0, right: 30.0),
+                          child: Image.asset(
+                              'assets/images/evening_stretching.png'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Card(
+                              elevation: 5,
+                              margin: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Image.asset(
+                                    'assets/logo.png',
+                                    height: 200,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          'Tesla Model S',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Price: \$XX,XXX',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Card(
+                              elevation: 5,
+                              margin: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Image.asset(
+                                    'assets/logo.png',
+                                    height: 200,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          'Tesla Model 3',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Price: \$XX,XXX',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Card(
+                              elevation: 5,
+                              margin: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Image.asset(
+                                    'assets/logo.png',
+                                    height: 200,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          'Tesla Model X',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Price: \$XX,XXX',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Card(
+                              elevation: 5,
+                              margin: EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Image.asset(
+                                    'assets/logo.png',
+                                    height: 200,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          'Tesla Model Y',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Price: \$XX,XXX',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ],
@@ -99,5 +328,4 @@ class HomeScreen extends StatelessWidget {
   }
 
   ProfileScreen() {}
-
 }
